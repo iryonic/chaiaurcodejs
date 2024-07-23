af8ade05ad857b3c5fe9815be6c3b707f78ae820
iryonic <drop.mail.iry@gmail.com>
2024-07-23 12:00:53 +0530

    second

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+ let accountEmail = "iryonic@gmail.com";
+ var accountPassword = "12345iry";
+
+
+ accountCity ="JAIPUR";
+
+ let accountState ;
+
+//  accountId = 134;
+ accountEmail ="me@meme.com"
+ accountPassword ="12vbfj";
+
+accountCity ="Reasi";
+ console.log(accountId);
+  console.table([accountId,accountEmail,accountPassword,accountCity,accountState ])
